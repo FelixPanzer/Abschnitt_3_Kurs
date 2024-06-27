e15bb828d4655b9e5fd43fc92545c98ab181e3ed
VMSCGDK <felixfrenkenberger@porscheinformatik.com>
2024-06-27 16:30:17 +0200

    Abschnitt 3 im Spring Kurs

---
+++ src/main/java/felix/springframework/spring6di/Spring6DiApplication.java
@@ -0,0 +1,23 @@
+package felix.springframework.spring6di;
+
+import felix.springframework.spring6di.controllers.MyController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Spring6DiApplication
+{
+
+    public static void main(String[] args)
+    {
+        ApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+        MyController controller = ctx.getBean(MyController.class);
+
+        System.out.println("In Main Method");
+
+        System.out.println(controller.sayHello());
+    }
+
+}
